882caaba8f2eb121b34ee1837fa6ad782e197662
Daniel Szemerey <szemy2@gmail.com>
2021-01-18 17:57:40 +0100

    feat: Passed all unittests and outperformed minimax with iterative depth
    - Migrated and modified the code for minimax search which I developed during the exercises
    - Added iterative deepening
    - Implemented a timertest (which was not needed in the end)

--- Adversarial Search/my_custom_player.py
+++ Adversarial Search/my_custom_player.py
@@ -1,6 +1,10 @@
 
 from sample_players import DataPlayer
 
+from isolation import DebugState
+
+import random
+import time
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -19,6 +23,15 @@ class CustomPlayer(DataPlayer):
       any pickleable object to the self.context attribute.
     **********************************************************************
     """
+
+
+    def __init__(self, player_id):
+        self.player_id = player_id
+        self.start_time = None
+        self.max_time = 140 ## in miliseconds
+        self.depth_limit = 10
+        self.timertest_off = True
+
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -42,5 +55,103 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        #
+        # self.queue.put(random.choice(state.actions()))
+        print('In get_action(), state received:')
+        debug_board = DebugState.from_state(state)
+        print(debug_board)
+        
+        
+        # self.queue.put(self.iterative_deepening(state, depth_limit=2))
+
+
+
+        ## With iterative deepening
+
+        for depth in range(1, self.depth_limit + 1):
+          self.queue.put(self.minimax(state, depth))
+
+        
+
+    def iterative_deepening(self, state, depth_limit):
+
+        self.start_time = time.time()
+        if state.terminal_test() or len(state.actions()) < 1: 
+            print("terminal test was true")
+            return state.locs[self.player_id]
+
+        
+        best_move = None
+        for depth in range(1, depth_limit + 1):
+            # print("==========")
+            # print("DEPTH = ", depth)
+            if self.timertest(): return best_move
+            best_move = self.minimax(state, depth)
+        
+
+        print(best_move)
+        return best_move
+
+    def timertest(self):
+        if self.timertest_off: return False
+        if (time.time() - self.start_time) * 1000 > self.max_time: return True ## in milliseconds
+        else: return False
+
+    def minimax(self, state, depth):
+        """ Return the move along a branch of the game tree that
+        has the best possible value.  A move is a pair of coordinates
+        in (column, row) order corresponding to a legal move for
+        the searching player.
+        
+        You can ignore the special case of calling this function
+        from a terminal state.
+        """
+        best_score = float("-inf")
+        best_move = random.choice(state.actions())
+        for a in state.actions():
+            if self.timertest(): return best_move
+            # call has been updated with a depth limit
+            v = self.min_value(state.result(a), depth - 1)
+            if v > best_score:
+                best_score = v
+                best_move = a
+        return best_move
+
+    def my_moves(self, state):
+        player_location = state.locs[self.player_id]
+        return len(state.liberties(player_location))
+
+    def min_value(self, state, depth):
+        """ Return the value for a win (+1) if the game is over,
+        otherwise return the minimum value over all legal child
+        nodes.
+        """
+        if state.terminal_test():
+            return state.utility(self.player_id)
+        
+        if depth <= 0 or self.timertest():
+            return self.my_moves(state)
+        
+        v = float("inf")
+        for a in state.actions():
+            # the depth should be decremented by 1 on each call
+            v = min(v, self.max_value(state.result(a), depth - 1))
+        return v
+
+
+    def max_value(self, state, depth):
+        """ Return the value for a loss (-1) if the game is over,
+        otherwise return the maximum value over all legal child
+        nodes.
+        """
+        if state.terminal_test():
+            return state.utility(self.player_id)
+        
+        if depth <= 0 or self.timertest():
+            return self.my_moves(state)
+        
+        v = float("-inf")
+        for a in state.actions():
+            # the depth should be decremented by 1 on each call
+            v = max(v, self.min_value(state.result(a), depth - 1))
+        return v
